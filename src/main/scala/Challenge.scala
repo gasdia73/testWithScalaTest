8c6b0bfea72771c6af91dc11166af714c2758cfc
Roberto Gasdia <roberto.gasdia@innoteam.it>
2025-09-29 10:47:14 +0200

    added test to challenge example

--- src/main/scala/Challenge.scala
+++ src/main/scala/Challenge.scala
@@ -3,6 +3,6 @@
   println(appMessage)
 
 def nTimes(f: Int => Int, n: Int): Int => Int = {
-  if (n == 1) f else 
+  if (n <= 1) f else 
   (x: Int) => nTimes(f, n - 1)(f(x))
 }
--- src/test/scala/ChallengeSuite.scala
+++ src/test/scala/ChallengeSuite.scala
@@ -9,4 +9,8 @@ class ChallengeSuite extends AnyFunSuite {
   test("1 times 20 * 20 should give 400") {
     assert(nTimes(_ * 20, 1)(20) == 400)
   }
+
+  test("negative or zero times equals once") {
+    assert(nTimes(_ * 20, -3)(20) == 400)
+  }  
 }
